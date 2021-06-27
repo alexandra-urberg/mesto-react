30294e6aea0f5cc8157321018bb98f0f849edf92
Sasha Stepanova <chernishevskie@yandex.ru>
2021-06-27 03:20:53 -0300

    feat:(11) linked Card and CurrentUserContext. Created variable, that checks is we are the owner of the card if not we won't see "trash" image

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,15 @@
-import React from 'react';
+import { useContext } from "react";
+import { CurrentUserContext } from '../contexts/CurrentUserContext.js';
 
-const Card = ({link, name, likes, onDeleteCard, onCardClick}) => {
+const Card = ({link, name, likes, owner, onDeleteCard, onCardClick}) => {
+
+    const currentUserId = useContext(CurrentUserContext)._id;
+
+    const isOwn = owner._id === currentUserId// Определяем, являемся ли мы владельцем текущей карточки
+
+    const cardDeleteButtonClassName = (// Создаём переменную, которую после зададим в `className` для кнопки удаления
+        `element__trash ${isOwn ? 'element__trash_visible' : ''}`
+    );
 
     const handleCardClick = () => {//обработчик передающий информауию от card в ImagePopup для открытия полноразмерной картинки
         onCardClick({name, link})
@@ -8,7 +17,7 @@ const Card = ({link, name, likes, onDeleteCard, onCardClick}) => {
 
     return (
         <li className="element template__card">
-            <button className="element__trash" onClick={onDeleteCard} type="button"></button>
+            <button className={cardDeleteButtonClassName} onClick={onDeleteCard} type="button"></button>
             <img className="element__image" onClick={handleCardClick} src={link} alt={name} />
             <div className="element__block">
                 <h2 className="element__quote">{name}</h2>
