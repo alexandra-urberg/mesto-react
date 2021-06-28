07a78e62ce30c56255f614001e397d3ecef44e63
Sasha Stepanova <chernishevskie@yandex.ru>
2021-06-28 19:56:18 -0300

    efactor:(11) new component from App which edits UserAvatarPopUp

---
+++ src/components/EditAvatarPopup.js
@@ -0,0 +1,40 @@
+import { useRef } from 'react';
+import PopupWithForm from './PopupWithForm';
+
+const EditAvatarPopup = (props) => {
+    const avatarRef = useRef(); // записываем объект, возвращаемый хуком, в переменную
+
+    function handleSubmit(e) {
+        e.preventDefault();
+      
+        props.onUpdateAvatar(//Передаём значения во внешний обработчик для измения аватара
+          avatarRef.current.value
+        );
+    } 
+
+    return (
+        <PopupWithForm
+            isOpen={props.isOpen}
+            onClose={props.onClose}
+            handleCloseByOverlay={props.handleCloseByOverlay}
+            onSubmit={handleSubmit}
+            name="avatar"
+            title="Обновить аватар"
+            btn="Сохранить"
+        >
+            <label className="popup__label">
+                <input
+                    ref={avatarRef}
+                    type="url"
+                    name="avatar"
+                    className="popup__input"
+                    placeholder="Ссылка на фотографию"
+                    required
+                />
+                <span className="input-avatar-error input-error"></span>
+            </label>
+        </PopupWithForm>
+    )
+}
+
+export default EditAvatarPopup;
